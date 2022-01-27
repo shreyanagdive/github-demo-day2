16614571be0c8c759a7ed293517d794eceee6d7b
Shreya Nagdive <65818149+shreyanagdive@users.noreply.github.com>
2022-01-27 12:04:30 +0530

    inital commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+//add implementation
+def add(x,y):
+    return x+y   # on bug123
+
+//substract implementation
+def subtract(x,y):
+    return x-y    #on main
+
+//multiply implementation
+def multiply(x,y):
+    return x*y
+
+//divide implementation
+def divide(x,y):
+
+    if y=0:
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
+
